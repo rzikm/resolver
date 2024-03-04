152fc82d3dad3955e5f36a0d198996f38774e09e
antonfirsov <antonfir@gmail.com>
2024-03-04 18:32:07 +0100

    Dockerfile for windows containers

--- Resolver.Tests/run-testserver.ps1
+++ Resolver.Tests/run-testserver.ps1
@@ -1,2 +1,9 @@
-docker build -t test-dns-server .
+param([switch]$w=$false)
+
+if ($w) {
+	docker build -t test-dns-server -f ./Dockerfile.windows .
+} else {
+	docker build -t test-dns-server .
+}
+
 docker run --rm -it -p 1053:53/udp -p 1053:53/tcp test-dns-server
