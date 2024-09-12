6ca0c360c29f87cc100f662672816ecbd81ac3ca
Radek Zikmund <r.zikmund.rz@gmail.com>
2024-09-12 15:43:01 +0200

    Add Benchmark project

---
+++ Resolver.Benchmarks/GlobalUsings.cs
@@ -0,0 +1,5 @@
+global using BenchmarkDotNet.Attributes;
+global using Test.Net;
+
+global using System.Net;
+global using System.Net.Sockets;
---
+++ Resolver.Benchmarks/ResolverBenchmark.cs
@@ -0,0 +1,28 @@
+[MemoryDiagnoser]
+public class ResolverVsOsBenchmark
+{
+    private const string Host = "google.com";
+
+    public ResolverVsOsBenchmark()
+    {
+        _resolver = new Resolver();
+        _resolver.Timeout = TimeSpan.FromSeconds(5);
+
+        _dnsClient = new LookupClient();
+    }
+
+    private readonly Resolver _resolver;
+    private readonly LookupClient _dnsClient;
+
+    [Benchmark]
+    public async Task<AddressResult[]> Resolver()
+    {
+        return await _resolver.ResolveIPAddressAsync(Host, AddressFamily.InterNetwork);
+    }
+
+    [Benchmark(Baseline = true)]
+    public async Task<IPAddress[]> OsDns()
+    {
+        return await Dns.GetHostAddressesAsync(Host, AddressFamily.InterNetwork);
+    }
+}
